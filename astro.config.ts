fffd171c249aead1e0aadf7834d068d890c89254
KeyLimePie <111712235+Prokid64@users.noreply.github.com>
2024-06-03 13:36:07 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -45,7 +45,8 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
+	site: "prokid64.github.io",
+  base: "/ghwebsite",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
